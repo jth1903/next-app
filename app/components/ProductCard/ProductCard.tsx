299c21d2bbfee8a43938841e52e9fe40032a7a89
= <jth1903@hotmail.com>
2024-04-16 15:09:43 -0500

    added basic styiling using tailwind and daisyUI

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,11 +1,10 @@
 'use client';
 import React from 'react'
 import AddToCart from './AddToCart';
-import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div >
       <AddToCart/>
     </div>
   )
